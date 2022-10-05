925dfcf1674aef7808ef18f3a7fb1fdfa01df1de
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2022-10-05 20:40:47 -0300

    acrescentando codigo

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-module.exports = {
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/pages/inicial.html',
-    }),
-  ],
-};
